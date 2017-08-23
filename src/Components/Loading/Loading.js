86b8af09de97c4ddc4a73c8bf01abf81a207781c
Margo Schaedel <margoschaedel@gmail.com>
2017-08-23 16:06:31 -0600

    loading page complete while fetch api runs

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -2,10 +2,12 @@ import React from 'react'
 import './Loading.css'
 
 const Loading = () => {
-
   return (
     <div className='loading-container'>
-    <h3>Hello World!</h3>
+      <div className="darth-image"></div>
+      <audio className='loading-audio' autoPlay>
+           <source src='http://www.thesoundarchive.com/starwars/swvader02.mp3' autoPlay/>
+      </audio>
     </div>
   )
 }
