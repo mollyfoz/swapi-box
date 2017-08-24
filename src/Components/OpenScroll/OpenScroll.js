c291b8b4259d2ccad90f2235687239683cb875c8
Molly Foster <mollyrfoster@gmail.com>
2017-08-23 21:04:51 -0600

    begin final styles WIP

--- src/Components/OpenScroll/OpenScroll.js
+++ src/Components/OpenScroll/OpenScroll.js
@@ -18,9 +18,7 @@ const OpenScroll = ({ filmData }) => {
           <p className='crawl-text'>{randomFilmObj.crawl}</p>
         </div>
       </div>
-      <audio className='opening-audio' autoPlay>
-           <source src='http://www.thesoundarchive.com/starwars/star-wars-theme-song.mp3' autoPlay/>
-         </audio>
+
     </div>
   )
 }
@@ -30,3 +28,7 @@ OpenScroll.propTypes = {
 }
 
 export default OpenScroll
+
+// <audio className='opening-audio' autoPlay>
+//      <source src='http://www.thesoundarchive.com/starwars/star-wars-theme-song.mp3' autoPlay/>
+//    </audio>
