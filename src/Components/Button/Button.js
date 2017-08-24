29bbb59423327154befbcb28c29a4c248d7b638e
Margo Schaedel <margoschaedel@gmail.com>
2017-08-23 22:17:16 -0600

    Add active class for subject buttons

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -2,10 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './Button.css'
 
-const Button = ({ subject, getSubjectData }) => {
+const Button = ({ subject, getSubjectData, currentSubject }) => {
+
+  const currentlyActive = (currentSubject === subject) ? 'subject-btn active-btn' : 'subject-btn'
 
   return (
-    <button className='subject-btn' onClick={ () => getSubjectData(subject) }>
+    <button className={ currentlyActive } onClick={ () => getSubjectData(subject) }>
     { subject }
     </button>
   )
@@ -13,7 +15,8 @@ const Button = ({ subject, getSubjectData }) => {
 
 Button.propTypes = {
   subject: PropTypes.string,
-  getSubjectData: PropTypes.func
+  getSubjectData: PropTypes.func,
+  currentSubject: PropTypes.string,
 }
 
 export default Button;
